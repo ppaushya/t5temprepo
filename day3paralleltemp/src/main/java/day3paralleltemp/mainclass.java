c49aa52053fa335790515aaeb9494a3d398c896f
deeptrip <deeptrip@din73003013.corp.capgemini.com>
2018-12-04 10:39:33 +0530

    main method updated

---
+++ day3paralleltemp/src/main/java/day3paralleltemp/mainclass.java
@@ -0,0 +1,19 @@
+package day3paralleltemp;
+
+public class mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		
+		int i;
+		
+		
+		for(i=1;i<10;i++)
+		{
+			System.out.println(i);
+		}
+		
+	}
+
+}
