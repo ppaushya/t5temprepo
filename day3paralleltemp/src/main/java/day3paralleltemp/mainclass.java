99f78d9e431bd3241ae94a0adfa0bc699fd8d310
deeptrip <deeptrip@din73003013.corp.capgemini.com>
2018-12-04 10:53:06 +0530

    4th msg

--- day3paralleltemp/src/main/java/day3paralleltemp/mainclass.java
+++ day3paralleltemp/src/main/java/day3paralleltemp/mainclass.java
@@ -4,16 +4,9 @@ public class mainclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     
-		
+    	
 		int i;
-		
-		
-		for(i=1;i<10;i++)
-		{
-			System.out.println(i);
-		}
-		
+	
 	  System.out.println("nikhil");
 	}
 
